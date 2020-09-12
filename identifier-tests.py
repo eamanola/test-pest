723cdcf35d0595a5812b166aec2b4936ba85325b
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2020-09-12 23:03:13 +0300

    initial commit

---
+++ identifier-tests.py
@@ -0,0 +1,364 @@
+import identifier
+
+debug = False;
+#debug = True;
+
+def test_show_files(
+file_name,
+expected_show_name,
+expected_season,
+expected_episode,
+expected_year,
+expected_remove_user_tags,
+expected_remove_meta_tags,
+expected_remove_file_extension,
+expected_is_media,
+expected_is_subtitle,
+expected_is_oad,
+expected_is_extra):
+    if identifier.guess_show_name(file_name) != expected_show_name:
+        print("guess_show_name:", file_name);
+        print("expected:", '"%s"' % expected_show_name);
+        print("got:", '"%s"' % identifier.guess_show_name(file_name));
+        print("--------------------------------------------------------------");
+
+    if identifier.guess_season(file_name) != expected_season:
+        print("guess_season", file_name);
+        print("expected:", expected_season);
+        print("got:", identifier.guess_season(file_name));
+        print("--------------------------------------------------------------");
+
+    if identifier.guess_episode(file_name) != expected_episode:
+        print("guess_episode", file_name);
+        print("expected:", expected_episode);
+        print("got:", identifier.guess_episode(file_name));
+        print("--------------------------------------------------------------");
+
+    if identifier.guess_year(file_name) != expected_year:
+        print("guess_year:", file_name);
+        print("expected:", expected_year);
+        print("got:", identifier.guess_year(file_name));
+        print("--------------------------------------------------------------");
+
+    if identifier.remove_user_tags(file_name) != expected_remove_user_tags:
+        print("remove_user_tags:", file_name);
+        print("expected:", '"%s"' % expected_remove_user_tags);
+        print("got:", '"%s"' % identifier.remove_user_tags(file_name));
+        print("--------------------------------------------------------------");
+
+    if identifier.remove_meta_tags(file_name) != expected_remove_meta_tags:
+        print("remove_meta_tags:", file_name);
+        print("expected:", '"%s"' % expected_remove_meta_tags);
+        print("got:", '"%s"' % identifier.remove_meta_tags(file_name));
+        print("--------------------------------------------------------------");
+
+    if identifier.remove_file_extension(file_name) != expected_remove_file_extension:
+        print("remove_file_extension:", file_name);
+        print("expected:", '"%s"' % expected_remove_file_extension);
+        print("got:", '"%s"' % identifier.remove_file_extension(file_name));
+        print("--------------------------------------------------------------");
+
+    if identifier.is_media(file_name) != expected_is_media:
+        print("is_media", file_name);
+        print("expected:", expected_is_media);
+        print("got:", identifier.is_media(file_name));
+        print("--------------------------------------------------------------");
+
+    if identifier.is_subtitle(file_name) != expected_is_subtitle:
+        print("is_subtitle:", file_name);
+        print("expected:", expected_is_subtitle);
+        print("got:", identifier.is_subtitle(file_name));
+        print("--------------------------------------------------------------");
+
+    if identifier.is_oad(file_name) != expected_is_oad:
+        print("is_oad:", file_name);
+        print("expected:", expected_is_oad);
+        print("got:", identifier.is_oad(file_name));
+        print("--------------------------------------------------------------");
+
+    if identifier.is_extra(file_name) != expected_is_extra:
+        print("is_extra:", file_name);
+        print("expected:", expected_is_extra);
+        print("got:", identifier.is_extra(file_name));
+        print("--------------------------------------------------------------");
+
+print('test_show_files:') if debug else ""
+test_shows = open("test-shows", "r");
+line = test_shows.readline();
+
+while line:
+    if line.startswith('#'):
+        print('-', line.strip()) if debug else ""
+        line = test_shows.readline();
+        continue;
+    elif not line.strip():
+        print('-', line.strip()) if debug else ""
+        line = test_shows.readline();
+        continue;
+    elif line.strip() == "EOF":
+        print('-', line.strip()) if debug else ""
+        break;
+
+    file_name = line.strip();
+    print('file_name:', file_name) if debug else ""
+    line = test_shows.readline()
+
+    expected_show_name = line.strip();
+    print('expected_show_name:', expected_show_name) if debug else ""
+    line = test_shows.readline();
+
+    expected_season = int(line.strip());
+    print('expected_season:', expected_season) if debug else ""
+    line = test_shows.readline();
+
+    expected_episode = int(line.strip());
+    print('expected_episode:', expected_episode) if debug else ""
+    line = test_shows.readline();
+
+    expected_year = int(line.strip());
+    print('expected_year:', expected_year) if debug else ""
+    line = test_shows.readline();
+
+    expected_remove_user_tags = line.strip();
+    print('expected_remove_user_tags:', expected_remove_user_tags) if debug else ""
+    line = test_shows.readline();
+
+    expected_remove_meta_tags = line.strip();
+    print('expected_remove_meta_tags:', expected_remove_meta_tags) if debug else ""
+    line = test_shows.readline();
+
+    expected_remove_file_extension = line.strip();
+    print('expected_remove_file_extension:', expected_remove_file_extension) if debug else ""
+    line = test_shows.readline();
+
+    expected_is_media = line.strip().lower() == "true";
+    print('expected_is_media:', expected_is_media) if debug else ""
+    line = test_shows.readline();
+
+    expected_is_subtitle = line.strip().lower() == "true";
+    print('expected_is_subtitle:', expected_is_subtitle) if debug else ""
+    line = test_shows.readline();
+
+    expected_is_oad = line.strip().lower() == "true";
+    print('expected_is_oad:', expected_is_oad) if debug else ""
+    line = test_shows.readline();
+
+    expected_is_extra = line.strip().lower() == "true";
+    print('expected_is_extra:', expected_is_extra) if debug else ""
+    line = test_shows.readline();
+
+    test_show_files(
+    file_name,
+    expected_show_name,
+    expected_season,
+    expected_episode,
+    expected_year,
+    expected_remove_user_tags,
+    expected_remove_meta_tags,
+    expected_remove_file_extension,
+    expected_is_media,
+    expected_is_subtitle,
+    expected_is_oad,
+    expected_is_extra);
+
+test_shows.close();
+
+print("") if debug else "";
+
+
+def test_movie_files(
+file_name,
+expected_show_name,
+expected_year
+):
+    if identifier.guess_show_name(file_name) != expected_show_name:
+        print('guess_show_name', file_name);
+        print('expected', '"%s"' % expected_show_name);
+        print('got', '"%s"' % identifier.guess_show_name(file_name));
+        print("--------------------------------------------------------------");
+
+    if identifier.guess_year(file_name) != expected_year:
+        print('guess_year', file_name);
+        print('expected', expected_year);
+        print('got', identifier.guess_year(file_name));
+        print("--------------------------------------------------------------");
+
+print('test_movie_files:') if debug else ""
+test_movies = open("test-movies", "r");
+line = test_movies.readline();
+
+while line:
+    if line.startswith('#'):
+        print('-', line.strip()) if debug else ""
+        line = test_movies.readline();
+        continue;
+    elif not line.strip():
+        print('-', line.strip()) if debug else ""
+        line = test_movies.readline();
+        continue;
+    elif line.strip() == "EOF":
+        print('-', line.strip()) if debug else ""
+        break;
+
+    file_name = line.strip();
+    print('file_name:', file_name) if debug else ""
+    line = test_movies.readline()
+
+    expected_show_name = line.strip();
+    print('expected_show_name:', expected_show_name) if debug else ""
+    line = test_movies.readline();
+
+    expected_year = int(line.strip());
+    print('expected_year:', expected_year) if debug else ""
+    line = test_movies.readline();
+
+    test_movie_files(
+        file_name,
+        expected_show_name,
+        expected_year
+    );
+
+test_movies.close();
+
+print("") if debug else "";
+
+print('guess_season unit:') if debug else ""
+season_test = (
+'show/Season 1/e1.mkv',
+'show/Season1/e1.mkv',
+'show/Season.1/e1.mkv',
+'show/S 1/e1.mkv',
+'show/S1/e1.mkv',
+'show/S.1/e1.mkv')
+for test in season_test:
+    print(test) if debug else ""
+
+    if identifier.guess_season(test) != 1:
+        print('guess_season', test);
+        print('expected', 1);
+        print('got', identifier.guess_season(test));
+
+print("") if debug else "";
+
+print('guess_episode unit:') if debug else ""
+episode_test = (
+'show/Season 1/episode 1.mkv',
+'show/Season 1/episode1.mkv',
+'show/Season 1/episode.1.mkv',
+'show/Season 1/e 1.mkv',
+'show/Season 1/e1.mkv',
+'show/Season 1/e.1.mkv')
+for test in episode_test:
+    print(test) if debug else ""
+
+    if identifier.guess_episode(test) != 1:
+        print('guess_episode', test);
+        print('expected', 1);
+        print('got', identifier.guess_episode(test));
+
+print("") if debug else "";
+
+print('is_extra & episode:') if debug else ""
+extras_test = (
+'show/extras/episode 1.mkv',
+'show/Season1/extra1.mkv',
+'show/Season1/extra 1.mkv',
+'show/Season1/extra.1.mkv')
+for test in extras_test:
+    print(test) if debug else ""
+
+    if identifier.guess_episode(test) != 1:
+        print('extras_test', test);
+        print('expected', 1);
+        print('got', identifier.guess_episode(test));
+
+    if not identifier.is_extra(test):
+        print('extras_test', test);
+        print('expected extra');
+        print('got not');
+
+print("") if debug else "";
+
+print('is_extra case sensitivity small:') if debug else ""
+test = 'show/Season1/extra1.mkv';
+print(test) if debug else ""
+if not identifier.is_extra(test):
+    print('is_extra case sensitivity small:', test);
+    print('expected extra');
+    print('got not');
+
+print("") if debug else "";
+
+print('is_extra case sensitivity mixed:') if debug else ""
+test = 'show/Season1/ExTRa1.mkv';
+print(test) if debug else ""
+if not identifier.is_extra(test):
+    print('is_extra case sensitivity mixed:', test);
+    print('expected extra');
+    print('got not');
+
+print("") if debug else "";
+
+print('is_extra case sensitivity capital:') if debug else ""
+test = 'show/Season1/EXTRA1.mkv';
+print(test) if debug else ""
+if not identifier.is_extra(test):
+    print('is_extra case sensitivity capital:', test);
+    print('expected extra');
+    print('got not');
+
+print("") if debug else "";
+
+print('is_oad & episode:') if debug else ""
+oad_test = (
+'show/OAD/episode 1.mkv',
+'show/Season1/OAD1.mkv',
+'show/Season1/OAD 1.mkv',
+'show/Season1/OAD.1.mkv')
+for test in oad_test:
+    print(test) if debug else ""
+
+    if identifier.guess_episode(test) != 1:
+        print('extras_test', test);
+        print('expected', 1);
+        print('got', identifier.guess_episode(test));
+
+    if not identifier.is_oad(test):
+        print('oad_test', test);
+        print('expected OAD');
+        print('got not');
+
+print("") if debug else "";
+
+print('is_oad case sensitivity small:') if debug else ""
+test = 'show/Season1/oad1.mkv';
+print(test) if debug else ""
+if identifier.is_oad(test):
+    print('is_extra case sensitivity small:', test);
+    print('expected not OAD');
+    print('got OAD');
+
+print("") if debug else "";
+
+print('is_oad case sensitivity small:') if debug else ""
+test = 'show/Season1/oAd1.mkv';
+print(test) if debug else ""
+if identifier.is_oad(test):
+    print('oad_test', test);
+    print('expected not OAD');
+    print('got OAD');
+
+print("") if debug else "";
+
+print('is_oad case sensitivity capital:') if debug else ""
+test = 'show/Season1/OAD1.mkv';
+print(test) if debug else ""
+if not identifier.is_oad(test):
+    print('is_oad case sensitivity capital', test);
+    print('expected OAD');
+    print('got not');
+
+print("") if debug else "";
+
+print('Completed Successfully');
+exit();
---
+++ identifier.py
@@ -0,0 +1,159 @@
+import os
+import re
+
+re_season = re.compile("(?:^|[^a-zA-Z]+)(?:season|s)(?:\s*|\.)(\d+)", re.IGNORECASE);
+re_oad = re.compile("OAD");
+re_extra = re.compile("extra", re.IGNORECASE);
+re_episode_oad = re.compile("OAD(?:\s*|\.)(\d+)");
+re_episode_extra = re.compile("extra(?:\s*|\.)(\d+)", re.IGNORECASE);
+re_episode = re.compile("(?:^|[^a-zA-Z]+)(?:episode|e|ep)(?:\s*|\.)(\d+)", re.IGNORECASE);
+re_episode2 = re.compile("(?:^|[^a-zA-Z0-9]+)(?:\s*)(\d{1,3})(?:[^a-zA-Z0-9]+|$)", re.IGNORECASE);
+re_metatags = re.compile("\([^\)]*\)\s*");
+re_year = re.compile("\s*(?:\((\d{4})\)|\[(\d{4})\]|(?:\s+|\.)(\d{4})(?:\s+|\.))\s*");
+re_usertags = re.compile("\[[^\]]*\]\s*");
+re_clean_show_name = (
+    re.compile("(?:720|1080)p(?:\s+|\.|-|$)", re.IGNORECASE),
+    #re.compile("720p(?:\s+|\.|-|$)", re.IGNORECASE),
+
+    re.compile("(?:web|hd|bd)rip(?:\s+|\.|$)", re.IGNORECASE),
+    #re.compile("hdrip(?:\s+|\.|$)", re.IGNORECASE),
+    re.compile("web-dl(?:\s+|\.|$)", re.IGNORECASE),
+
+    re.compile("\d+mb(?:\s+|\.|-|$)", re.IGNORECASE),
+
+    re.compile("x26(?:4|5)(?:\s+|\.|-|$)", re.IGNORECASE),
+    re.compile("hvec(?:\s+|\.|-|$)", re.IGNORECASE),
+    re.compile("xvid(?:\s+|\.|-|$)", re.IGNORECASE),
+    re.compile("aac(?:\s+|\.|-|$)", re.IGNORECASE),
+    re.compile("ac3(?:.?(?:evo))?(?:\s+|\.|-|$)", re.IGNORECASE),
+    re.compile("aac5.1(?:\s+|\.|-|$)", re.IGNORECASE),
+    re.compile("flac", re.IGNORECASE),
+
+    re.compile("\d{4}(?:\s+|\.|-|$)", re.IGNORECASE)
+);
+
+UNKNOWN_SEASON = 0;
+UNKNOWN_EPISODE = 0;
+UNKNOWN_YEAR = 0;
+MEDIA_EXTENSIONS = ('.mkv', '.mp4','.avi');
+SUB_TITLE_EXTENSIONS = ('.srt',);
+FILE_EXTENSIONS = MEDIA_EXTENSIONS + SUB_TITLE_EXTENSIONS;
+
+def clean_show_name(show_name):
+    for clean in re_clean_show_name:
+        show_name = clean.sub("", show_name);
+
+    return show_name;
+
+def guess_show_name(file):
+    parts = file.split(os.path.sep)
+
+    show_name = parts[0];
+    show_name = remove_meta_tags(show_name)
+    show_name = remove_user_tags(show_name)
+    show_name = remove_file_extension(show_name)
+    show_name = clean_show_name(show_name)
+
+    show_name = show_name.replace(".", " ");
+
+    return show_name.strip();
+
+def guess_season(file):
+    parts = file.split(os.path.sep);
+    parts.reverse();
+
+    season = UNKNOWN_SEASON;
+
+    for part in parts:
+        has_season = re_season.search(part);
+
+        if has_season:
+            season = has_season.group(1);
+            break;
+
+    return int(season);
+
+def guess_episode(file):
+    parts = remove_meta_tags(remove_user_tags(file)).split(os.path.sep);
+    parts.reverse();
+
+    episode = UNKNOWN_EPISODE;
+
+    for part in parts:
+        has_episode = re_episode.search(part);
+
+        if has_episode:
+            episode = has_episode.group(1);
+            break;
+
+        else:
+            has_episode = re_episode2.search(clean_show_name(part));
+
+            if has_episode:
+                episode = has_episode.group(1);
+                break;
+
+            elif is_extra(part):
+                has_episode = re_episode_extra.search(part);
+
+                if has_episode:
+                    episode = has_episode.group(1);
+                    break;
+
+            elif is_oad(part):
+                has_episode = re_episode_oad.search(part);
+
+                if has_episode:
+                    episode = has_episode.group(1);
+                    break;
+
+    return int(episode);
+
+def guess_year(file):
+    parts = file.split(os.path.sep);
+
+    year = UNKNOWN_YEAR;
+
+    for part in parts:
+        has_year = re_year.search(part);
+
+        if has_year:
+            if has_year.group(1):
+                year = has_year.group(1);
+            elif has_year.group(2):
+                year = has_year.group(2);
+            elif has_year.group(3):
+                year = has_year.group(3);
+            else:
+                year = UNKNOWN_YEAR;
+            break;
+
+    return int(year);
+
+def remove_user_tags(file):
+    return re_usertags.sub("",  file);
+
+def remove_meta_tags(file):
+    return re_metatags.sub("", file);
+
+def remove_file_extension(file):
+    file_name, extension = os.path.splitext(file);
+
+    if extension in FILE_EXTENSIONS:
+        file = file_name;
+
+    return file;
+
+def is_media(file):
+    file_name, extension = os.path.splitext(file);
+    return extension in MEDIA_EXTENSIONS;
+
+def is_subtitle(file):
+    file_name, extension = os.path.splitext(file);
+    return extension in SUB_TITLE_EXTENSIONS;
+
+def is_oad(file):
+    return re_oad.search(remove_user_tags(remove_meta_tags(file))) != None;
+
+def is_extra(file):
+    return re_extra.search(remove_user_tags(remove_meta_tags(file))) != None;
