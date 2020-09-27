f512900af3c11e551d930028c9962a84323e977a
Ehsun Amanolahi <ehsun.amanolahi@gmail.com>
2020-09-27 21:46:20 +0300

    Container.id(), MediaLibrary, Contentainer.parent()

--- classes/container.py
+++ classes/container.py
@@ -1,17 +1,21 @@
 from classes.identifiable import Identifiable
+import hashlib
 
 
 class Container(object):
-    def __init__(self, container_name):
+    def __init__(self, parent=None):
         super(Container, self).__init__()
 
         self.containers = []
         self.media = []
-        self.container_name = container_name
+        self._parent = parent
 
-    def get_container(self, container_name):
+    def parent(self):
+        return self._parent
+
+    def get_container(self, container_id):
         for container in self.containers:
-            if container.container_name == container_name:
+            if container.id() == container_id:
                 return container
 
         return None
@@ -23,28 +27,44 @@ class Container(object):
 
         return None
 
+    def id(hash_id):
+        return hashlib.md5(hash_id.encode('utf-8')).hexdigest()
 
-class Show(Container, Identifiable):
-    def __init__(self, show_name):
-        super(Show, self).__init__(show_name)
+
+class MediaLibrary(Container):
+    def __init__(self, path, parent=None):
+        super(MediaLibrary, self).__init__(parent)
+        self._path = path
+
+    def path(self):
+        return self._path
+
+    def id(self):
+        return Container.id(self.path())
+
+
+class Show(MediaLibrary, Identifiable):
+    def __init__(self, library_path, show_name, parent=None):
+        super(Show, self).__init__(library_path, parent)
+        self._show_name = show_name
 
     def show_name(self):
-        return self.container_name
+        return self._show_name
 
     def seasons(self):
         return self.containers
 
+    def id(self):
+        return Container.id(self.show_name())
+
 
 class Season(Show):
-    def __init__(self, season_name, season_number):
-        super(Season, self).__init__(season_name)
+    def __init__(self, library_path, show_name, season_number, parent=None):
+        super(Season, self).__init__(library_path, show_name, parent)
         self._season_number = season_number
 
     def seasons(self):
         return None
-
-    def season_name(self):
-        return self.container_name
 
     def extras(self):
         return self.containers
@@ -55,10 +75,27 @@ class Season(Show):
     def season_number(self):
         return self._season_number
 
+    def id(self):
+        id_str = "{}{}{}".format(
+            self.show_name(), "season", self.season_number()
+        )
+        return Container.id(id_str)
+
 
 class Extra(Season):
-    def __init__(self, extra_name):
-        super(Extra, self).__init__(extra_name, None)
+    def __init__(self, library_path, show_name, season_number, parent=None):
+        super(Extra, self).__init__(
+            library_path,
+            show_name,
+            season_number,
+            parent
+        )
 
     def extras(self):
         return None
+
+    def id(self):
+        id_str = "{}{}{}{}".format(
+            self.show_name(), "season", self.season_number(), "extra"
+        )
+        return Container.id(id_str)
--- tests/container-tests.py
+++ tests/container-tests.py
@@ -1,4 +1,4 @@
-from classes.container import Container, Show, Season, Extra
+from classes.container import Container, MediaLibrary, Show, Season, Extra
 from classes.media import Media
 
 debug = True
@@ -10,27 +10,44 @@ RANDOM_INT = 22
 FAIL = "FAIL"
 PASS = "PASS"
 
+LIBRARY_PATH = RANDOM_STR
+SHOW_NAME = RANDOM_STR2
+SEASON_NUMBER = RANDOM_INT
+
 test_name = "isheritance:"
 print(test_name) if debug else ""
 
 test_name = "Container()"
 print(test_name) if debug else ""
 
-container = Container(RANDOM_STR)
+container = Container()
 if (
     not isinstance(container, Container) or
+    isinstance(container, MediaLibrary) or
     isinstance(container, Show) or
     isinstance(container, Season) or
     isinstance(container, Extra)
 ):
     print(test_name, FAIL)
 
+test_name = "MediaLibrary()"
+print(test_name) if debug else ""
+
+mediaLibrary = MediaLibrary(LIBRARY_PATH)
+if (
+    not isinstance(mediaLibrary, (Container, MediaLibrary)) or
+    isinstance(mediaLibrary, Show) or
+    isinstance(mediaLibrary, Season) or
+    isinstance(mediaLibrary, Extra)
+):
+    print(test_name, FAIL)
+
 test_name = "Show()"
 print(test_name) if debug else ""
 
-show = Show(RANDOM_STR)
+show = Show(LIBRARY_PATH, SHOW_NAME)
 if (
-    not isinstance(show, (Container, Show)) or
+    not isinstance(show, (Container, MediaLibrary, Show)) or
     isinstance(container, Season) or
     isinstance(container, Extra)
 ):
@@ -39,9 +56,9 @@ if (
 test_name = "Season()"
 print(test_name) if debug else ""
 
-season = Season(RANDOM_STR, RANDOM_INT)
+season = Season(LIBRARY_PATH, SHOW_NAME, SEASON_NUMBER)
 if (
-    not isinstance(season, (Container, Show, Season)) or
+    not isinstance(season, (Container, MediaLibrary, Show, Season)) or
     isinstance(container, Extra)
 ):
     print(test_name, FAIL)
@@ -49,33 +66,29 @@ if (
 test_name = "Extra()"
 print(test_name) if debug else ""
 
-extra = Extra(RANDOM_STR)
-if not isinstance(extra, (Container, Show, Season, Extra)):
+extra = Extra(LIBRARY_PATH, SHOW_NAME, SEASON_NUMBER)
+if not isinstance(extra, (Container, MediaLibrary, Show, Season, Extra)):
     print(test_name, FAIL)
 
 test_name = "Container.get_container"
 print(test_name) if debug else ""
 
-container = Container(RANDOM_STR)
-
-if container.get_container(RANDOM_STR2) is not None:
-    print(test_name, FAIL)
-
-container2 = Container(RANDOM_STR2)
-container.containers.append(container2)
-
-if not container.get_container(RANDOM_STR2) == container2:
-    print(test_name, FAIL)
+container = MediaLibrary(LIBRARY_PATH)
 
 if container.get_container(RANDOM_STR) is not None:
-    print(test_name, FAIL)
+    print(test_name, FAIL, 1)
+
+container2 = Show(LIBRARY_PATH, SHOW_NAME)
+container.containers.append(container2)
+if not container.get_container(container2.id()) == container2:
+    print(test_name, FAIL, 2)
+
+if container.get_container(RANDOM_STR) is not None:
+    print(test_name, FAIL, 3)
 
 test_name = "Container.get_media"
 print(test_name) if debug else ""
-container = Container(RANDOM_STR)
-
-if container.get_container(RANDOM_STR2) is not None:
-    print(test_name, FAIL, 1)
+container = Container()
 
 media = Media(RANDOM_STR2)
 container.media.append(media)
@@ -86,58 +99,103 @@ if not container.get_media(RANDOM_STR2) == media:
 if container.get_media(RANDOM_STR) is not None:
     print(test_name, FAIL, 3)
 
+test_name = "Container.parent"
+print(test_name) if debug else ""
+container = Container()
+if (
+    container.parent() is not None and
+    container._parent is not None
+):
+    print(test_name, FAIL)
+
+parent = Container()
+container = Container(parent)
+if (
+    not container.parent() == parent or
+    not container.parent() == container._parent
+):
+    print(test_name, FAIL)
+
+test_name = "MediaLibrary.path()"
+print(test_name) if debug else ""
+mediaLibrary = MediaLibrary(LIBRARY_PATH)
+if (
+    not mediaLibrary.path() == LIBRARY_PATH or
+    not mediaLibrary.path() == mediaLibrary._path
+):
+    print(test_name, FAIL)
+
 test_name = "Show.show_name"
 print(test_name) if debug else ""
-show = Show(RANDOM_STR)
+show = Show(LIBRARY_PATH, SHOW_NAME)
 if (
-    not show.show_name() == RANDOM_STR or
-    not show.show_name() == show.container_name
+    not show.show_name() == SHOW_NAME or
+    not show.show_name() == show._show_name
 ):
     print(test_name, FAIL)
 
 test_name = "Show.seasons"
 print(test_name) if debug else ""
-show = Show(RANDOM_STR)
+show = Show(LIBRARY_PATH, SHOW_NAME)
 if not show.seasons() == show.containers:
     print(test_name, FAIL)
 
 test_name = "Season.season_number"
 print(test_name) if debug else ""
-season = Season(RANDOM_STR, RANDOM_INT)
-if not season.season_number() == RANDOM_INT:
+season = Season(LIBRARY_PATH, SHOW_NAME, SEASON_NUMBER)
+if not season.season_number() == SEASON_NUMBER:
     print(test_name, FAIL)
 
 test_name = "Season.seasons"
 print(test_name) if debug else ""
-season = Season(RANDOM_STR, RANDOM_INT)
+season = Season(LIBRARY_PATH, SHOW_NAME, SEASON_NUMBER)
 if season.seasons() is not None or season.seasons() == season.containers:
-    print(test_name, FAIL)
-
-test_name = "Season.season_name"
-print(test_name) if debug else ""
-season = Season(RANDOM_STR, RANDOM_INT)
-if (
-    not season.season_name() == RANDOM_STR or
-    not season.season_name() == season.container_name
-):
     print(test_name, FAIL)
 
 test_name = "Season.extras"
 print(test_name) if debug else ""
-season = Season(RANDOM_STR, RANDOM_INT)
+season = Season(LIBRARY_PATH, SHOW_NAME, SEASON_NUMBER)
 if not season.extras() == season.containers:
     print(test_name, FAIL)
 
 test_name = "Season.episodes"
 print(test_name) if debug else ""
-season = Season(RANDOM_STR, RANDOM_INT)
+season = Season(LIBRARY_PATH, SHOW_NAME, SEASON_NUMBER)
 if not season.episodes() == season.media:
     print(test_name, FAIL)
 
 test_name = "extra.extras"
 print(test_name) if debug else ""
-extra = Extra(RANDOM_STR)
+extra = Extra(LIBRARY_PATH, SHOW_NAME, SEASON_NUMBER)
 if extra.extras() is not None or extra.extras() == extra.containers:
     print(test_name, FAIL)
+
+test_name = "id"
+
+print(test_name) if debug else ""
+mediaLibrary = MediaLibrary(LIBRARY_PATH)
+if Container.id(mediaLibrary.path()) != mediaLibrary.id():
+    print(test_name, FAIL)
+
+show = Show(LIBRARY_PATH, SHOW_NAME)
+if Container.id(show.show_name()) != show.id():
+    print(test_name, FAIL)
+
+season = Season(LIBRARY_PATH, SHOW_NAME, SEASON_NUMBER)
+if (
+    Container.id("{}{}{}".format(
+        season.show_name(), "season", season.season_number()
+    )) != season.id()
+):
+    print(test_name, FAIL)
+
+extra = Extra(LIBRARY_PATH, SHOW_NAME, SEASON_NUMBER)
+if (
+    Container.id("{}{}{}{}".format(
+        extra.show_name(), "season", extra.season_number(), "extra"
+    )) != extra.id()
+):
+    print(test_name, FAIL)
+
 
 print("container-tests: Successfully Completed")
